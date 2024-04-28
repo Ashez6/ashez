afd353f58dc2e37eca15b99b3705a64418204297
samalouty <128055267+samalouty@users.noreply.github.com>
2024-04-28 17:31:28 +0300

    Created Tempelate pop out

--- Organization/scriptFull.js
+++ Organization/scriptFull.js
@@ -1,10 +1,12 @@
 const deleteButts = document.querySelectorAll(".delete-button");
 const jobBox = document.querySelectorAll(".job-box");
 
-console.log("hello");
-
 for (let i = 0; i < deleteButts.length; i++) {
   deleteButts[i].addEventListener("click", (e) => {
     jobBox[i].remove();
   });
 }
+
+const popup1 = document.getElementById("pop-out1");
+
+popup1.addEventListener("click", (e) => {});

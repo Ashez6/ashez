613dc1919e1c8426da93794404e18c951cb3ed44
samalouty <128055267+samalouty@users.noreply.github.com>
2024-04-28 19:19:37 +0300

    Made the pop up buttons clickable

--- Organization/scriptFull.js
+++ Organization/scriptFull.js
@@ -7,6 +7,17 @@ for (let i = 0; i < deleteButts.length; i++) {
   });
 }
 
-const popup1 = document.getElementById("pop-out1");
+const viewDonation1 = document.getElementById("vdonation1"); //
+const viewDonation1Screen = document.getElementById("vdonation1-screen");
 
-popup1.addEventListener("click", (e) => {});
+viewDonation1.addEventListener("click", (e) => {
+  viewDonation1Screen.style.visibility = "visible";
+  viewDonation1Screen.style.display = "";
+});
+
+const CloseDonation1 = document.getElementById("close-donation-1");
+
+CloseDonation1.addEventListener("click", (e) => {
+  viewDonation1Screen.style.display = "none";
+  viewDonation1Screen.style.visibility = "hidden";
+});

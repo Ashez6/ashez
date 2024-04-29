325be31d7d2933ba864f0b63ef85c06372d8c5bc
samalouty <128055267+samalouty@users.noreply.github.com>
2024-04-29 18:41:09 +0300

    Teacher and Doctor redirection
    used localStorage to preserve the type of the logged in user

---
+++ login.js
@@ -0,0 +1,50 @@
+const usernameInput = document.getElementById("login");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const enteredUsername = usernameInput.value;
+  const enteredPassword = passwordInput.value;
+
+  const users = {
+    admin: { username: "admin", password: "admin1" },
+    donor: { username: "donor", password: "donor1" },
+    teacher: { username: "teach", password: "teach1" },
+    org: { username: "org", password: "org1" },
+    doctor: { username: "doc", password: "doc1" },
+  };
+
+  const matchedUser = Object.values(users).find(
+    (user) =>
+      user.username === enteredUsername && user.password === enteredPassword
+  );
+
+  if (matchedUser) {
+    // Redirect based on user role
+    switch (matchedUser.username) {
+      case "admin":
+        window.location.href = "Admin/admin_home.html";
+        break;
+      case "donor":
+        window.location.href = "Donor/homepage.html";
+        localStorage.setItem("type", "donor");
+        break;
+      case "teach":
+        window.location.href = "Donor/homepage.html";
+        localStorage.setItem("type", "teacher");
+        break;
+      case "doc":
+        window.location.href = "Donor/homepage.html";
+        localStorage.setItem("type", "doctor");
+        break;
+      case "org":
+        window.location.href = "Organization/homepageorg.html";
+        localStorage.setItem("type", "organization");
+        break;
+    }
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
+});

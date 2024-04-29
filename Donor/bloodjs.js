d44b005e3c2bd1bbdb918e35fe10cd6a8adb89bc
Aby293 <abdelrahmanelaby@gmail.com>
2024-04-29 22:43:28 +0300

    active handled

--- Donor/bloodjs.js
+++ Donor/bloodjs.js
@@ -1,4 +1,69 @@
 
+//Filter
+// const checkboxes = document.querySelectorAll('.length-filter');
+// const items = document.getElementsByClassName('job-box');
+// const search=document.querySelector('#contact-submit');
+// search.addEventListener('click',(e)=>{
+//   const selectedOptions = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+//   for (let i = 0; i < items.length; i++) {
+//     const item = items[i];
+//     const itemName = item.textContent.toLowerCase();
+//     if (selectedOptions.length > 0) {
+//       if (selectedOptions.includes('short') && itemLength > 5) {
+//         shouldDisplay = false;
+//       }
+//       if (selectedOptions.includes('medium') && (itemLength <= 5 || itemLength > 10)) {
+//         shouldDisplay = false;
+//       }
+//       if (selectedOptions.includes('long') && itemLength <= 10) {
+//         shouldDisplay = false;
+//       }
+//     }
+
+//     item.style.display = shouldDisplay ? 'inline-block' : 'none';
+//   }
+// })
+
+const keywordsInput = document.getElementById('keywords');
+const hospitalCheckboxes = document.querySelectorAll('.filter1');
+const filterButton = document.getElementById('contact-submit');
+function applyFilters() {
+  const keywords = keywordsInput.value.toLowerCase();
+  const selectedHospitals = Array.from(hospitalCheckboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value.toLowerCase());
+
+  // Filter logic here
+  const jobBoxes = document.querySelectorAll('.job-box');
+  jobBoxes.forEach(jobBox => {
+      const hospitalName = jobBox.querySelector('.job-content').textContent.toLowerCase();
+      console.log(hospitalName);
+      const shouldDisplay = matchKeywords(keywords, hospitalName) && matchHospital(selectedHospitals, hospitalName);
+      console.log(shouldDisplay);
+      jobBox.style.display = shouldDisplay ? '' : 'none';
+      jobBox.style.visible = shouldDisplay ? '' : 'none';
+  });
+}
+
+function matchKeywords(keywords, text) {
+  return keywords === '' || text.includes(keywords);
+}
+
+function matchHospital(selectedHospitals, hospitalName) {
+  if(selectedHospitals.length === 0){
+    return true;
+  }
+  for(let i=0;i<selectedHospitals.length;i++){
+    if(hospitalName.includes(selectedHospitals[i])){
+      return true;
+    }
+  }
+  return false;
+}
+
+filterButton.addEventListener('click', applyFilters);
+
+
 // View Donation 1
 const viewDonation1 = document.getElementById("vdonation1"); //
 const viewDonation1Screen = document.getElementById("vdonation1-screen");

02d46150cad47af1baa7a57d7aa6a69a9520f5f3
Aby293 <abdelrahmanelaby@gmail.com>
2024-04-30 19:10:35 +0300

    change photo button in organisation

--- Organization/scriptReq.js
+++ Organization/scriptReq.js
@@ -1,3 +1,26 @@
+
+const photobtns= document.querySelectorAll(".photobtn");
+const photos= document.querySelectorAll(".photo");
+
+  
+for (let i = 0; i < photobtns.length; i++) {
+  photobtns[i].addEventListener('change', (e)=>handleFileSelect(e,i));
+}
+
+// Function to handle file selection
+function handleFileSelect(event,i) {
+  const file = event.target.files[0]; // Get the selected file
+  const reader = new FileReader();
+  reader.onload = function(event) {
+      // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
+      photos[i].src = event.target.result; // Update the image source with the selected file
+  };
+
+  // Read the selected file as a data URL
+  reader.readAsDataURL(file);
+}
+
+
 const deleteButts = document.querySelectorAll(".delete-button");
 const jobBox = document.querySelectorAll(".job-box");
 

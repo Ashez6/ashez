6b8824c9f953ce60a09cd54d91530e5fabb037aa
Aby293 <abdelrahmanelaby@gmail.com>
2024-05-01 12:03:24 +0300

    alerts in admin

--- Admin/organization2.js
+++ Admin/organization2.js
@@ -1,4 +1,8 @@
 
+const deldiv= document.getElementById("deldiv");
+const newdiv=document.createElement("div");
+deldiv.append(newdiv);
+
 //Filter
 
 const keywordsInput = document.getElementById('keywords');
@@ -7,6 +11,7 @@ const filter2Checkboxes = document.querySelectorAll('.filter2');
 const filter3Checkboxes = document.querySelectorAll('.filter3');
 const filterButton = document.getElementById('contact-submit');
 function applyFilters() {
+  newdiv.innerHTML="";
   const keywords = keywordsInput.value.toLowerCase();
   const selected1 = Array.from(filter1Checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value.toLowerCase());
   const selected2 = Array.from(filter2Checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value.toLowerCase());
@@ -47,6 +52,7 @@ const viewDonation1 = document.getElementById("vdonation1"); //
 const viewDonation1Screen = document.getElementById("vdonation1-screen");
 
 viewDonation1.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation1Screen.style.visibility = "visible";
   viewDonation1Screen.style.display = "";
 });
@@ -54,6 +60,7 @@ viewDonation1.addEventListener("click", (e) => {
 const CloseDonation1 = document.getElementsByClassName("close-donation")[0];
 
 CloseDonation1.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation1Screen.style.display = "none";
   viewDonation1Screen.style.visibility = "hidden";
 });
@@ -64,6 +71,7 @@ const viewDonation2 = document.getElementById("vdonation2"); //
 const viewDonation2Screen = document.getElementById("vdonation2-screen");
 
 viewDonation2.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation2Screen.style.visibility = "visible";
   viewDonation2Screen.style.display = "";
 });
@@ -71,6 +79,7 @@ viewDonation2.addEventListener("click", (e) => {
 const CloseDonation2 = document.getElementsByClassName("close-donation")[1];
 
 CloseDonation2.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation2Screen.style.display = "none";
   viewDonation2Screen.style.visibility = "hidden";
 });
@@ -81,6 +90,7 @@ const viewDonation3 = document.getElementById("vdonation3"); //
 const viewDonation3Screen = document.getElementById("vdonation3-screen");
 
 viewDonation3.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation3Screen.style.visibility = "visible";
   viewDonation3Screen.style.display = "";
 });
@@ -88,6 +98,7 @@ viewDonation3.addEventListener("click", (e) => {
 const CloseDonation3 = document.getElementsByClassName("close-donation")[2];
 
 CloseDonation3.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation3Screen.style.display = "none";
   viewDonation3Screen.style.visibility = "hidden";
 });
@@ -98,6 +109,7 @@ const viewDonation4 = document.getElementById("vdonation4"); //
 const viewDonation4Screen = document.getElementById("vdonation4-screen");
 
 viewDonation4.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation4Screen.style.visibility = "visible";
   viewDonation4Screen.style.display = "";
 });
@@ -105,6 +117,7 @@ viewDonation4.addEventListener("click", (e) => {
 const CloseDonation4 = document.getElementsByClassName("close-donation")[3];
 
 CloseDonation4.addEventListener("click", (e) => {
+  newdiv.innerHTML="";
   viewDonation4Screen.style.display = "none";
   viewDonation4Screen.style.visibility = "hidden";
 });
@@ -113,36 +126,41 @@ CloseDonation4.addEventListener("click", (e) => {
 
 const delete1 = document.getElementById("delete1");
 
-  delete1.addEventListener("click", (e) => {
-  alert("Organization Deleted from the system ");
+
+delete1.addEventListener("click", (e) => {
   const del = document.getElementById("org1");
   viewDonation1Screen.remove();
   del.remove();
+  newdiv.innerHTML='<div class="alert alert-success alert-white rounded" id="successful"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-check"></i></div><strong>Success!</strong> Organisation has been deleted successfully.</div>';
 });
 
 const delete2 = document.getElementById("delete2");
 
   delete2.addEventListener("click", (e) => {
-  alert("Organization Deleted from the system ");
   const del = document.getElementById("org2");
   viewDonation2Screen.remove();
   del.remove();
+  newdiv.innerHTML='<div class="alert alert-success alert-white rounded" id="successful"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-check"></i></div><strong>Success!</strong> Organisation has been deleted successfully.</div>';
+
 });
 
 const delete3 = document.getElementById("delete3");
 
   delete3.addEventListener("click", (e) => {
-  alert("Organization Deleted from the system ");
+
   const del = document.getElementById("org3");
   viewDonation3Screen.remove();
   del.remove();
+  newdiv.innerHTML='<div class="alert alert-success alert-white rounded" id="successful"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-check"></i></div><strong>Success!</strong> Organisation has been deleted successfully.</div>';
+
 });
 
 const delete4 = document.getElementById("delete4");
 
   delete4.addEventListener("click", (e) => {
-  alert("Organization Deleted from the system ");
   const del = document.getElementById("org4");
   viewDonation4Screen.remove();
   del.remove();
+  newdiv.innerHTML='<div class="alert alert-success alert-white rounded" id="successful"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-check"></i></div><strong>Success!</strong> Organisation has been deleted successfully.</div>';
+
 });

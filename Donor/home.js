f5dfb22ef7499663a957adf3cace1867a7cc79cc
samalouty <128055267+samalouty@users.noreply.github.com>
2024-05-04 00:45:33 +0300

    Added back button

--- Donor/home.js
+++ Donor/home.js
@@ -12,5 +12,3 @@ document.addEventListener("DOMContentLoaded", (e) => {
       "<li><a href='allsectors.html'>All</a><a href='medicalsector.html'>Medical Supplies</a><a href='schoolsector.html'>School Supplies</a><a href='clothessector.html'>Clothes</a><a href='foodsector.html'>Food</a><a href='bloodsector.html'>Blood</a><a href='toyssector.html'>Toys</a><a href='doctor.html'>Pro-Bono Cases Requests</a></ul>";
   }
 });
-
-

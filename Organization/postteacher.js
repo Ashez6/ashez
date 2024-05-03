0fda0242ac87ac1b94b6ab7b6e5e429f187ca85f
samalouty <128055267+samalouty@users.noreply.github.com>
2024-05-03 23:09:55 +0300

    Added validation to postteacher.html

--- Organization/postteacher.js
+++ Organization/postteacher.js
@@ -1,19 +1,77 @@
-const photobtns= document.querySelectorAll(".photobtn");
-const photos= document.querySelectorAll(".photo");
+const photobtns = document.querySelectorAll(".photobtn");
+const photos = document.querySelectorAll(".photo");
 
-  
-  photobtns[0].addEventListener('change', (e)=>handleFileSelect(e,0));
-
+photobtns[0].addEventListener("change", (e) => handleFileSelect(e, 0));
 
 // Function to handle file selection
-function handleFileSelect(event,i) {
+function handleFileSelect(event, i) {
   const file = event.target.files[0]; // Get the selected file
   const reader = new FileReader();
-  reader.onload = function(event) {
-      // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
-      photos[i].src = event.target.result; // Update the image source with the selected file
+  reader.onload = function (event) {
+    // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
+    photos[i].src = event.target.result; // Update the image source with the selected file
   };
 
   // Read the selected file as a data URL
   reader.readAsDataURL(file);
 }
+
+let errorMessage = "";
+
+function checkInput(inputElement) {
+  inputElement.style.border = "";
+  if (inputElement.value === "") {
+    inputElement.style.border = "1px solid red";
+    errorMessage += `Please enter the ${inputElement.id}.<br>`;
+  }
+}
+
+const postReq = document.getElementById("update-profile");
+
+postReq.addEventListener("click", (e) => {
+  const title = document.getElementById("title");
+  const noOfStudents = document.getElementById("number-studs");
+  const address = document.getElementById("address");
+  const subject = document.getElementById("subject");
+
+  errorMessage = "";
+
+  checkInput(title);
+  checkInput(noOfStudents);
+  checkInput(address);
+  checkInput(subject);
+
+  const fileInput = document.getElementById("file-here");
+  if (!fileInput.value) {
+    errorMessage += "Please upload a photo.<br>";
+    e.preventDefault();
+  }
+
+  if (count == 0) {
+    errorMessage += "Please add a pin to your address<br>";
+  }
+
+  const newDiv = document.createElement("div");
+  const appendH = document.getElementById("append-here");
+
+  if (errorMessage == "") {
+    newDiv.innerHTML =
+      '<div class="alert alert-success alert-white rounded"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-check"></i></div><strong>Success!</strong> Your Request has been posted successfully!</div>';
+
+    if (appendH.hasChildNodes()) {
+      appendH.removeChild(appendH.firstChild);
+    }
+
+    appendH.appendChild(newDiv);
+  } else {
+    newDiv.innerHTML =
+      '<div class="alert alert-danger alert-white rounded"><button type="button" data-dismiss="alert" aria-hidden="true" class="close">×</button><div class="icon"><i class="fa fa-times-circle"></i></div><strong>Failure!<br></strong>' +
+      errorMessage +
+      "</div>";
+
+    if (appendH.hasChildNodes()) {
+      appendH.removeChild(appendH.firstChild);
+    }
+    appendH.appendChild(newDiv);
+  }
+});

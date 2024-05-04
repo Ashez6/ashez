0d3c8f9921ae79dcfba71dfc07426b2188ab10e0
samalouty <128055267+samalouty@users.noreply.github.com>
2024-05-04 14:13:58 +0300

    Type = Book handled in Post School
    fixed some houba typos as well

--- Organization/postschool.js
+++ Organization/postschool.js
@@ -1,17 +1,15 @@
-const photobtns= document.querySelectorAll(".photobtn");
-const photos= document.querySelectorAll(".photo");
+const photobtns = document.querySelectorAll(".photobtn");
+const photos = document.querySelectorAll(".photo");
 
-  
-  photobtns[0].addEventListener('change', (e)=>handleFileSelect(e,0));
-
+photobtns[0].addEventListener("change", (e) => handleFileSelect(e, 0));
 
 // Function to handle file selection
-function handleFileSelect(event,i) {
+function handleFileSelect(event, i) {
   const file = event.target.files[0]; // Get the selected file
   const reader = new FileReader();
-  reader.onload = function(event) {
-      // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
-      photos[i].src = event.target.result; // Update the image source with the selected file
+  reader.onload = function (event) {
+    // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
+    photos[i].src = event.target.result; // Update the image source with the selected file
   };
 
   // Read the selected file as a data URL
@@ -28,12 +26,33 @@ function checkInput(inputElement) {
   }
 }
 
+const dropdown = document.getElementById("type");
+const bookElem = document.getElementsByClassName("book-elem");
+
+dropdown.addEventListener("change", function () {
+  if (dropdown.value === "School Books") {
+    localStorage.setItem("booki", "yes");
+
+    for (let i = 0; i < bookElem.length; i++) {
+      bookElem[i].style.display = "block";
+    }
+  } else {
+    localStorage.setItem("booki", "no");
+
+    for (let i = 0; i < bookElem.length; i++) {
+      bookElem[i].style.display = "none";
+    }
+  }
+});
+
 const postReq = document.getElementById("update-profile");
 
 postReq.addEventListener("click", (e) => {
   const title = document.getElementById("title");
   const type = document.getElementById("type");
   const quantity = document.getElementById("quantity");
+  let booki = localStorage.getItem("booki");
+
   errorMessage = "";
 
   checkInput(title);
@@ -49,6 +68,25 @@ postReq.addEventListener("click", (e) => {
   if (!fileInput.value) {
     errorMessage += "Please upload a photo.<br>";
     e.preventDefault();
+  }
+
+  if (booki == "yes") {
+    const bookName = document.getElementById("bname");
+    const author = document.getElementById("author");
+    const language = document.getElementById("language");
+    const edition = document.getElementById("edition");
+    const summary = document.getElementById("summary");
+
+    bookName.style.border = "";
+    if (bookName.value === "") {
+      bookName.style.border = "1px solid red";
+      errorMessage += `Please enter the book name.<br>`;
+    }
+
+    checkInput(author);
+    checkInput(language);
+    checkInput(edition);
+    checkInput(summary);
   }
 
   const newDiv = document.createElement("div");

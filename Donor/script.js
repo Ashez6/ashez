2aee31a83e9dc732f02d488de2fb6f5742cf1dfc
samalouty <128055267+samalouty@users.noreply.github.com>
2024-05-11 11:20:48 +0300

    Removed back button, Added PP
    added profile pictures in org settings and donor settings and removed back button in all pages

--- Donor/script.js
+++ Donor/script.js
@@ -1,3 +1,21 @@
+const photobtns = document.querySelectorAll(".photobtn");
+const photos = document.querySelectorAll(".photo");
+
+photobtns[0].addEventListener("change", (e) => handleFileSelect(e, 0));
+
+// Function to handle file selection
+function handleFileSelect(event, i) {
+  const file = event.target.files[0]; // Get the selected file
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    // const imgHolder = document.querySelector('.img-holder.photo'); // Get the image holder element
+    photos[i].src = event.target.result; // Update the image source with the selected file
+  };
+
+  // Read the selected file as a data URL
+  reader.readAsDataURL(file);
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
   const navBar = document.querySelector(".nav-drop");
   const label = document.querySelector("#remove-label");
